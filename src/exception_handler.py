2b7828c1344adfa30a8cd7940373562d508ee660
sam <meysam.or.us@gmail.com>
2023-03-12 15:44:04 +0330

    Create exception handler and logger files

---
+++ src/exception_handler.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def error_message_detail(err_message, err_detail:sys):
+	_, _, exc_tb = err_detail.exc_info()
+	file_name = exc_tb.tb_frame.f_code.co_filename
+
+	error_message = "Error occured in Python script [{0}] line number [{1}] error message [{2}]".format(
+		file_name, exc_tb.tb_lineno, str(err_message)
+	)
+	return error_message
+
+
+class CustomException(Exception):
+	def __init__(self, err_message, err_detail: sys):
+		super().__init__(err_message)
+		self.error_message = error_message_detail(err_message=err_message, err_detail=err_detail)
+
+	def __str__(self):
+		return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+
+log_file_name = f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log"
+logs_directory_path = os.path.join(os.getcwd(), 'logs', log_file_name)
+os.makedirs(name=logs_directory_path, exist_ok=True)
+log_file_path = os.path.join(logs_directory_path, log_file_name)
+
+logging.basicConfig(filename=log_file_path,
+                    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s- %(message)s',
+                    level=logging.INFO
+                    )
+
